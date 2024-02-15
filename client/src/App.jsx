0a4611dbcbf7459853bd35da8e96c2c8a02894c5
SAMAKAKI <nikitos.shorick@gmail.com>
2024-02-15 20:27:41 +0100

    create pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,25 @@
+/* eslint-disable react/jsx-no-undef */
 /* eslint-disable no-unused-vars */
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import About from './pages/About'
+import Dashboard from './pages/Dashboard'
+import Projects from './pages/Projects'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+import Home from './pages/Home'
+
 const App = () => {
   return (
-    <h1 className="text-3xl font-bold">Hello</h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/dashboard' element={<Dashboard />} />
+        <Route path='/projects' element={<Projects />} />
+        <Route path='/sign-in' element={<SignIn />} />
+        <Route path='/sign-up' element={<SignUp />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
