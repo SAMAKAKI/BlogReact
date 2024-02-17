5d616aa261445749ee0ab3129ca052a65409c975
SAMAKAKI <nikitos.shorick@gmail.com>
2024-02-17 16:41:32 +0100

    create a test api route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import mongoose from 'mongoose'
 import doenv from 'dotenv'
+import userRoutes from './routes/user.routes.js';
 
 doenv.config()
 
@@ -15,3 +16,5 @@ const app = express()
 app.listen(3000, () => {
   console.log('Server running on port 3000')
 })
+
+app.use('/api/user', userRoutes);
