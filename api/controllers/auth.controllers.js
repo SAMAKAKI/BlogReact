88655cfa21c7afc12a27a98cc0dd55de8c5b5c36
SAMAKAKI <nikitos.shorick@gmail.com>
2024-02-17 18:29:38 +0100

    create a middleware and a function to handle errors

--- api/controllers/auth.controllers.js
+++ api/controllers/auth.controllers.js
@@ -1,11 +1,12 @@
 import User from "../models/user.model.js";
 import bcryptjs from 'bcryptjs'
+import { errorHandler } from "../utils/error.js";
 
-export const signUp = async (req, res) => {
+export const signUp = async (req, res, next) => {
   const { username, email, password } = req.body;
 
   if(!username || !email || !password || username === '' || email === '' || password === '')
-    return res.status(400).json({ message: 'All fields are required' })
+    next(errorHandler(400, 'All fields are required'))
 
   const hashedPassword = bcryptjs.hashSync(password, 10);
 
@@ -19,7 +20,7 @@ export const signUp = async (req, res) => {
     await newUser.save()
     return res.status(200).json({ message: 'create a new User' })  
   } catch (error) {
-    return res.status(500).json({ message: error })
+    next(error)
   }
 
 }
--- api/index.js
+++ api/index.js
@@ -13,11 +13,20 @@ mongoose.connect(process.env.MONGO).then(() => {
 })
 
 const app = express()
+
 app.use(express.json());
+app.use((err, req, res, next) => {
+  const statusCode  = err.statusCode || 500
+  const message = err.message || 'Internal Server Error'
+  res.status(statusCode).json({
+    success: false,
+    statusCode: statusCode,
+    message: message,
+  })
+})
+app.use('/api/user', userRoutes);
+app.use('/api/auth', authRoutes);
 
 app.listen(3000, () => {
   console.log('Server running on port 3000')
 })
-
-app.use('/api/user', userRoutes);
-app.use('/api/auth', authRoutes);
