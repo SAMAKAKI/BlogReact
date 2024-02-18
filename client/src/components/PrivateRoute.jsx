03e5763aabc3b04f30f03b300856564138392d2c
SAMAKAKI <nikitos.shorick@gmail.com>
2024-02-18 22:26:47 +0100

    make the dashboard private

---
+++ client/src/components/PrivateRoute.jsx
@@ -0,0 +1,10 @@
+/* eslint-disable no-unused-vars */
+import { useSelector } from 'react-redux'
+import { Outlet, Navigate } from 'react-router-dom'
+
+const PrivateRoute = () => {
+  const { currentUser } = useSelector(state => state.user)
+  return currentUser ? <Outlet /> : <Navigate to='/sign-in' />
+}
+
+export default PrivateRoute
