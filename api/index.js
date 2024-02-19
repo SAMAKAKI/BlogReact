68ecf924d2eefebca2ca4507784cf2b4c735a47b
SAMAKAKI <nikitos.shorick@gmail.com>
2024-02-19 18:49:21 +0100

    add update user api route

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ import doenv from 'dotenv'
 import userRoutes from './routes/user.routes.js';
 import authRoutes from './routes/auth.routes.js';
 import cors from 'cors'
+import cookieParser from 'cookie-parser';
 
 doenv.config()
 
@@ -16,6 +17,7 @@ mongoose.connect(process.env.MONGO).then(() => {
 const app = express()
 app.use(cors())
 app.use(express.json());
+app.use(cookieParser())
 app.use((err, req, res, next) => {
   const statusCode  = err.statusCode || 500
   const message = err.message || 'Internal Server Error'
