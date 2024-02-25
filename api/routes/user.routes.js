a62e11abc64db7851a8f326f2be1161364578bc6
SAMAKAKI <nikitos.shorick@gmail.com>
2024-02-25 09:49:14 +0100

    create get users api route

--- api/routes/user.routes.js
+++ api/routes/user.routes.js
@@ -1,11 +1,11 @@
 import express from 'express';
-import { test, updateUser, deleteUser, signOut } from '../controllers/user.controller.js';
+import { updateUser, deleteUser, signOut, getUsers } from '../controllers/user.controller.js';
 import { verifyToken } from '../utils/verifyUser.js';
 
 
 const router = express.Router();
 
-router.get('/test', test);
+router.get('/getUsers', verifyToken, getUsers)
 router.put('/update/:userId', verifyToken, updateUser)
 router.delete('/delete/:userId', verifyToken, deleteUser)
 router.post('/sign-out', signOut)
