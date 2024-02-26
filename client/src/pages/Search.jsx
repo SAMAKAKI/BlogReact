5b466d38b4ca91f29f9a1b026f40c92481d057c7
SAMAKAKI <nikitos.shorick@gmail.com>
2024-02-26 19:53:26 +0100

    THE END

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -136,7 +136,7 @@ const Search = () => {
       </div>
       <div className="w-full">
         <h1 className='text-3xl font-semibold sm:border-b border-gray-500 p-3 mt-5'>Posts results:</h1>
-        <div className="p-7 flex-wrap gap-4">
+        <div className="p-7 flex flex-wrap gap-4">
           {!loading && posts.length === 0 && <p className='text-xl text-gray-500'>No posts found</p>}
           {loading && (<p className='text-xl text-gray-500'>Loading...</p>)}
           {!loading && posts && posts.map((post) => (
