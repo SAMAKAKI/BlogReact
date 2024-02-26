7b0024f01fb395d1c126ecb5c881f1b338f1e68d
SAMAKAKI <nikitos.shorick@gmail.com>
2024-02-26 19:40:12 +0100

    complete about and projects pages

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,6 +1,18 @@
+/* eslint-disable react/no-unescaped-entities */
 const About = () => {
   return (
-    <div>About</div>
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="max-w-2xl mx-auto p-3 text-center">
+        <div>
+          <h1 className="text-3xl font-semibold my-7">About SAMAKAKI' Blog</h1>
+          <div className="text-md text-gray-500 flex flex-col gap-6">
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sint laudantium voluptate molestias quidem eaque officia vitae. Sequi, suscipit cum. Incidunt, odit. Alias consectetur possimus labore similique molestias velit voluptatibus rerum!</p>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sint laudantium voluptate molestias quidem eaque officia vitae. Sequi, suscipit cum. Incidunt, odit. Alias consectetur possimus labore similique molestias velit voluptatibus rerum!</p>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sint laudantium voluptate molestias quidem eaque officia vitae. Sequi, suscipit cum. Incidunt, odit. Alias consectetur possimus labore similique molestias velit voluptatibus rerum!</p>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
--- client/src/pages/Projects.jsx
+++ client/src/pages/Projects.jsx
@@ -1,6 +1,12 @@
+import CallToAction from "../components/CallToAction"
+
 const Projects = () => {
   return (
-    <div>Projects</div>
+    <div className="min-h-screen max-w-2xl mx-auto flex justify-center items-center flex-col gap-6 p-3">
+      <h1 className="text-3xl font-semibold">Projects</h1>
+      <p className="text-md text-gray-500 text-center">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officia nam ipsum eveniet mollitia odit, eligendi, quasi nemo eos dignissimos rem ab tempora id esse saepe ratione ullam voluptates! Earum, excepturi?</p>
+      <CallToAction />
+    </div>
   )
 }
 
